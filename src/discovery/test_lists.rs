03633cd355121c88b37d6c1db2fb5cac4f813f24
Me <me@me.me>
2018-12-28 20:22:52 -0800

    Outline TestLists public methods

--- src/discovery/test_lists.rs
+++ src/discovery/test_lists.rs
@@ -17,9 +17,9 @@ impl TestLists {
 	 */
 	pub fn new() -> TestLists {
 		TestLists {
-			_main_tests: vec!(),
-			_parallel_tests: vec!(),
-			_ignored_tests: vec!(),
+			_main_tests: vec![],
+			_parallel_tests: vec![],
+			_ignored_tests: vec![],
 		}
 	}
 
@@ -38,16 +38,34 @@ impl TestLists {
 	 */
 	pub fn add_main_test(&mut self, main_test: u64) {
 		//Sanity check: Is this already in parallel_tests?
-		//If so:
-		//	Report error, and return here.
-		//	(This is probably being called during macro execution,
-		//	so we don't want to halt all other macros)
+		let is_in_parallel_tests = true;
+		if is_in_parallel_tests {
+			//If so:
+			//	Report error, and return here.
+			//	(This is probably being called during macro execution,
+			//	so we don't want to halt all other macros)
+			let error_message = format!(
+				"Can't add test {} to main test list: test is already in parallel tests list",
+				main_test
+			);
+			unimplemented!();
 
+			return;
+		}
 		//Else:
-		//	Add the test to main_tests.
+		else {
+			//	Add the test to main_tests.
+			unimplemented!();
+		}
 
 		//DEBUG ASSERT: test is now in main_tests
-		unimplemented!()
+		let is_in_main_tests = false;
+		unimplemented!();
+		debug_assert!(
+			is_in_main_tests,
+			"Test {} is not in main tests list",
+			main_test
+		);
 	}
 
 	/**
@@ -62,16 +80,34 @@ impl TestLists {
 	 */
 	pub fn add_test(&mut self, test: u64) {
 		//Sanity check: Is this already in main_tests?
-		//If so:
-		//	Report error, and return here.
-		//	(This is probably being called during macro execution,
-		//	so we don't want to halt all other macros)
+		let is_in_main_tests = true;
+		if is_in_main_tests {
+			//If so:
+			//	Report error, and return here.
+			//	(This is probably being called during macro execution,
+			//	so we don't want to halt all other macros)
+			let error_message = format!(
+				"Can't add test {} to parallel test list: test is already in main tests list",
+				test
+			);
+			unimplemented!();
 
+			return;
+		}
 		//Else:
-		//	Add the test to parallel_tests.
+		else {
+			//	Add the test to parallel_tests.
+			unimplemented!();
+		}
 
 		//DEBUG ASSERT: test is now in parallel_tests
-		unimplemented!()
+		let is_in_parallel_tests = false;
+		unimplemented!();
+		debug_assert!(
+			is_in_parallel_tests,
+			"Test {} is not in parallel tests list",
+			test
+		);
 	}
 
 	/**
@@ -87,13 +123,35 @@ impl TestLists {
 	pub fn ignore_test(&mut self, test: u64) {
 		//Remove test from main_tests and parallel_tests,
 		//if it exists in either list.
+		unimplemented!();
 
 		//DEBUG ASSERT: test is not in main_tests
+		let is_in_main_tests = true;
+		unimplemented!();
+		debug_assert!(
+			!is_in_main_tests,
+			"Test {} is still in main tests list despite being ignored",
+			test
+		);
 		//DEBUG ASSERT: test is not in parallel_tests
+		let is_in_parallel_tests = true;
+		unimplemented!();
+		debug_assert!(
+			!is_in_parallel_tests,
+			"Test {} is still in parallel tests list despite being ignored",
+			test
+		);
 
 		//Add the test to ignored_tests.
+		unimplemented!();
 
 		//DEBUG ASSERT: test is now in ignored_tests
-		unimplemented!()
+		let is_in_ignored_tests = false;
+		unimplemented!();
+		debug_assert!(
+			is_in_ignored_tests,
+			"Test {} is not in ignored tests list",
+			test
+		);
 	}
 }
